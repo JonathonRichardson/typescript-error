fd0d187d497e8d25ac5baa0f06c7f8b32ac740f4
jon <jonathon.m.richardson@gmail.com>
2017-01-21 15:40:38 -0600

    Setup basic typescript skeleton.

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp');
+const jasmine = require('gulp-jasmine');
+const Reporter = require('jasmine-console-reporter');
+const merge = require('merge2');
+var ts = require("gulp-typescript");
+
+gulp.task("build", function () {
+    var tsProject = ts.createProject("tsconfig.json");
+    var compiled = tsProject.src().pipe(tsProject());
+
+    var js  = compiled.js;
+    var dts = compiled.dts;
+
+    var dest = gulp.dest(".");
+
+    return merge([
+        js.pipe(dest),
+        dts.pipe(dest)
+    ]);
+});
+
+gulp.task('test', ['build'], function() {
+    gulp.src('spec/**/*.js')
+        .pipe(jasmine({
+            reporter: new Reporter()
+        }));
+});
